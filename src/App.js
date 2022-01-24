bff3e5b3b65f1bcb5c5cbad5af2cd5da11c32810
Victoria Baeva <baeva3112@gmail.com>
2022-01-24 21:29:16 +0300

    Верстка
    Каркас главной страницы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+    return <div className='app-wrapper'>
+
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' alt=''/>
+        </header>
+
+        <nav className='nav'>
+            <div><a href="#">Profile</a></div>
+            <div><a href="#">Messages</a></div>
+            <div><a href="#">News</a></div>
+            <div><a href="#">Music</a></div>
+            <div><a href="#">Settings</a></div>
+        </nav>
+
+        <div className='content'>
+
+            <div>
+                <img src="https://miro.medium.com/max/1200/1*HsFBOH0ncItzjWJgLIuVIg.png" alt=""/>
+            </div>
+
+            <div>
+                ava +discriptiion
+            </div>
+
+            <div>
+                My Posts
+                <div>New Post</div>
+
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+
     </div>
-  );
-}
+};
 
 export default App;
