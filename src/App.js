ad81aba2ad22774cd22b210207882321504dae4a
Victoria Baeva <baeva3112@gmail.com>
2022-01-27 20:50:59 +0300

    Разбивка на компоненты

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 
 const App = () => {
-    return <div className='app-wrapper'>
+    return (
 
-        <header className='header'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' alt=''/>
-        </header>
+     <div className='app-wrapper'>
 
-        <nav className='nav'>
-            <div><a href="#">Profile</a></div>
-            <div><a href="#">Messages</a></div>
-            <div><a href="#">News</a></div>
-            <div><a href="#">Music</a></div>
-            <div><a href="#">Settings</a></div>
-        </nav>
-
-        <div className='content'>
-
-            <div>
-                <img src="https://miro.medium.com/max/1200/1*HsFBOH0ncItzjWJgLIuVIg.png" alt=""/>
-            </div>
-
-            <div>
-                ava +discriptiion
-            </div>
-
-            <div>
-                My Posts
-                <div>New Post</div>
-
-                <div>
-                    <div>Post1</div>
-                    <div>Post2</div>
-                </div>
-            </div>
-        </div>
+        <Header />
+        <Navbar />
+        <Profile />
 
     </div>
+    )
 };
 
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,28 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+
+            <div>
+                <img src="https://miro.medium.com/max/1200/1*HsFBOH0ncItzjWJgLIuVIg.png" alt=""/>
+            </div>
+
+            <div>
+                ava +discriptiion
+            </div>
+
+            <div>
+                My Posts
+                <div>New Post</div>
+
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
